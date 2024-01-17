b8187ac5e1888683afc4dd9d473c75e58f1ea423
Adrezej <125516792+Adrezej@users.noreply.github.com>
2024-01-17 19:20:21 +0200

    Delete static directory

--- static/script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('scroll', function() {
-    let header = document.querySelector('h1');
-    if (header) {
-        let scrolled = window.scrollY;
-        header.style.transform = 'translateY(' + scrolled * 0.3 + 'px)';
-    }
-});
